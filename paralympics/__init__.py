6bee1beee694185ea0e0485fa3eb7688fc298b07
YueYang101 <zcemuex@ucl.ac.uk>
2024-02-12 03:29:11 +0000

    11

--- paralympics/__init__.py
+++ paralympics/__init__.py
@@ -1,8 +1,10 @@
+# flask --app paralympics run --debug
 import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from flask_marshmallow import Marshmallow
 
 
 # https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/quickstart/
@@ -12,7 +14,9 @@ class Base(DeclarativeBase):
 
 # First create the db object using the SQLAlchemy constructor.
 # Pass a subclass of either DeclarativeBase or DeclarativeBaseNoMeta to the constructor.
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
+# Create a global Flask-Marshmallow object
+ma = Marshmallow()
 
 
 def create_app(test_config=None):
@@ -42,6 +46,7 @@ def create_app(test_config=None):
 
     # Initialise Flask with the SQLAlchemy database extension
     db.init_app(app)
+    ma.init_app(app)
 
     # Models are defined in the models module, so you must import them before calling create_all, otherwise SQLAlchemy
     # will not know about them.
--- paralympics/paralympics.py
+++ paralympics/paralympics.py
@@ -4,3 +4,52 @@ from flask import current_app as app
 @app.route('/')
 def hello():
     return f"Hello!"
+
+from paralympics.schemas import RegionSchema, EventSchema
+
+# Flask-Marshmallow Schemas
+regions_schema = RegionSchema(many=True)
+region_schema = RegionSchema()
+events_schema = EventSchema(many=True)
+event_schema = EventSchema()
+
+
+# Use route and specify the HTTP method(s). If you do not specify the methods then it will default to GET.
+@app.route('/something', methods=['GET', 'POST'])
+def something():
+    pass
+
+
+# Use Flask shortcut methods for each HTTP method `.get`, `.post`, `.delete`, `.patch`, `.put`
+@app.get('/something')
+def something():
+    pass
+
+from paralympics import db
+from paralympics.models import Region
+
+@app.get("/regions")
+def get_regions():
+    """Returns a list of NOC region codes and their details in JSON."""
+    # Select all the regions using Flask-SQLAlchemy
+    all_regions = db.session.execute(db.select(Region)).scalars()
+    # Get the data using Marshmallow schema (returns JSON)
+    result = regions_schema.dump(all_regions)
+    # Return the data
+    return result
+
+
+from paralympics.models import Event
+
+@app.get("/events/<event_id>")
+def get_event(event_id):
+    """ Returns the event with the given id JSON.
+
+    :param event_id: The id of the event to return
+    :param type event_id: int
+    :returns: JSON
+    """
+    event = db.session.execute(
+        db.select(Event).filter_by(id=event_id)
+    ).scalar_one_or_none()
+    return event_schema.dump(event)
